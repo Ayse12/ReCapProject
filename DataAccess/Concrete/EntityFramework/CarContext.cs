c125035408ab692f19bbe08d57ef801b922a5e2b
AYŞE İLHANLI <20162013027@std.nisantasi.edu.tr>
2021-03-15 14:39:59 +0300

    -Generic IEntityRepository -Car, Brand ve Color nesneleri için Entity Framework altyapısını

---
+++ DataAccess/Concrete/EntityFramework/CarContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //DESKTOP - B3G39MA\SQLEXPRESS02
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-B3G39MA\SQLEXPRESS02; Database=Car;Trusted_Connection=true");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
